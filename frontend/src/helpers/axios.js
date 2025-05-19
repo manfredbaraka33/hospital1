df5143cc0a7beb64e3dfd8747357a5ec22bdcc8a
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-05-19 11:05:58 +0300

    Update axios.js

--- frontend/src/helpers/axios.js
+++ frontend/src/helpers/axios.js
@@ -4,7 +4,7 @@ import { getAccessToken, getRefreshToken, getUser } from "../hooks/user.actions"
 
 // Create an Axios instance with default settings
 const axiosService = axios.create({
-  baseURL: "http://localhost:8001/api", // Update with your API's base URL
+  baseURL: "https://hospital1-drab.vercel.app/api", // Update with your API's base URL
   headers: {
     "Content-Type": "multipart/form-data",
   },
@@ -28,7 +28,7 @@ const refreshAuthLogic = async (failedRequest) => {
         refresh: getRefreshToken(),
       },
       {
-        baseURL: "http://127.0.0.1:8001/api", // Same API base URL
+        baseURL: "https://hospital1-drab.vercel.app/api", // Same API base URL
       }
     );
 
