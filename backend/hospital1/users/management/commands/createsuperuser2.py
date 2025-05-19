1eb777cd8c1de2eb29d6c2731910cd31410c11cc
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-05-19 09:43:15 +0300

    Update createsuperuser2.py

--- backend/hospital1/users/management/commands/createsuperuser2.py
+++ backend/hospital1/users/management/commands/createsuperuser2.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin1", "admin1@example.com", "1234")
+            User.objects.create_superuser("admin1@example.com", "1234")
             self.stdout.write("Superuser created.")
         else:
             self.stdout.write("Superuser already exists.")
