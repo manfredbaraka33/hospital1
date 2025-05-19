b84dd2986756b3983ef8b6a589e1e465d5cd9a37
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-05-19 09:25:50 +0300

    Update settings.py

--- backend/hospital1/hospital1/settings.py
+++ backend/hospital1/hospital1/settings.py
@@ -166,13 +166,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-import os
+
 # Define the location for media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
